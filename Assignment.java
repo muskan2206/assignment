6183ba34521a820c5a5b0b756298fe7b310ed03c
muskan2206 <54302160+muskan2206@users.noreply.github.com>
2020-05-22 15:25:18 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,12 @@
+package com.test;
+import java.util.*;
+public class Assignment {
+	public static void main(String[] args) {
+		char a,b;
+		HashSet<Character> d=new HashSet<Character>();
+		a=d.keySet().iterator().next().charAt(9);
+		b=d.keySet().iterator().next().charAt(27);
+		int x=((int)a+(int)b)/2;
+	}
+
+}
